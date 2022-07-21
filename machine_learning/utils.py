208c09ef06013e2b548744b029b6a83798cbbb0a
Duy Dien Nguyen <diennguyenduy1311999@gmail.com>
2022-07-21 11:00:45 +0200

    fix: change version of model

--- machine_learning/utils.py
+++ machine_learning/utils.py
@@ -3,7 +3,7 @@ import joblib
 
 def predict_hf(data):
 
-    trained_model = joblib.load("model/model.pkl")
+    trained_model = joblib.load("model/LogisticRegression_model.pkl")
     predictions = trained_model.predict(data)
 
     return predictions
